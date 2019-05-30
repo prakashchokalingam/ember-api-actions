56d2301fca7a2d350867f0d246b51b2275b8c98d
Prakash Chokalingam <cpvicky33@gmail.com>
2019-05-30 16:25:57 +0530

    Optional path changes

--- addon/utils/build-url.js
+++ addon/utils/build-url.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
-const { assert } = Ember;
+//const { assert } = Ember;
 
 export function buildOperationUrl(record, opPath, urlType, instance=true) {
-  assert('You must provide a path for instanceOp', opPath);
+  //assert('You must provide a path for instanceOp', opPath);
   let modelName = record.constructor.modelName || record.constructor.typeKey;
   let adapter = record.store.adapterFor(modelName);
-  let path = opPath;
+  let path = opPath || '';
   let snapshot = record._createSnapshot();
   let baseUrl = adapter.buildURL(modelName, instance ? record.get('id') : null, snapshot, urlType);
 
